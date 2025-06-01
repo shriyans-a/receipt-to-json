9a653b8907c02ef6fde49b2f752dc689bd31de21
Shriyans Ayalasomayajula <shriyans@Shriyanss-MacBook-Pro.local>
2025-05-31 22:05:48 -0700

    Removed key details

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
+# import necessary tools
 from google.cloud import vision
 from google.oauth2 import service_account
 import json
+import re
 
 credentials = service_account.Credentials.from_service_account_file("receipt_ocr_key.json")
 client = vision.ImageAnnotatorClient(credentials=credentials)
@@ -27,19 +29,71 @@ def parse_receipt(text): # define the function that takes an argument, text.
 
     # initialize result dictionary #
     result = {
-        "vendor": lines[0] if lines else "",
+        "vendor": lines[0] if lines else "", # assume vendor to be the first line
         "items": [],
+        "subtotal": "",
+        "tax": "",
         "total": ""
     }
 
+    promo_keywords = ["win", "gift card", "sweepstakes", "prize", "contest"] # list of promo words found on receipts. 
+    skip_keywords = ["subtotal", "total", "tax", "visa", "card", "tender", "store", "sold", "returned", "auth"] # list of other words to be skipped.
+    last_line = "" # empty string for use later.
+
     for line in lines: # Loop through every line in the receipt
-        if "total" in line.lower(): # check if the word 'total' is in the line
-            result["total"] = line # if the word 'total' is present, add the value to the dictionary next to the "total" row. 
-        elif "$" in line: # check if the dollar sign symbol ($) is in the line. 
-            result["items"].append(line) # if '$' is present, add that line to items in the dictionary. 
+        clean_line = line.strip() # remove leading and following whitespace
+        lower_line = clean_line.lower()
+
+
+        if any(keyword in lower_line for keyword in promo_keywords): # skip promotional content in the receipt.
+            continue
+
+        if "subtotal" in lower_line: # check for the word "subtotal" in the line. 
+            next_line_l = lines.index(line) + 1 # get the index of the next line after finding our current one.
+            if next_line_l < len(lines): 
+                next_line = lines[next_line_l].strip() # gets the next line by using the index found in the previous bit. 
+                if "$" in next_line: 
+                    result["subtotal"] = next_line # append the amount to the end. 
+            continue
+
+        if "tax" in lower_line: # repeated same logic as in finding the subtotal. 
+            next_line_l = lines.index(line) + 1
+            if next_line_l < len(lines):
+                next_line = lines[next_line_l].strip()
+                if "$" in next_line:
+                    result["tax"] = next_line
+            continue
+
+        if "total" in lower_line: # repeated same logic as in finding the subtotal. 
+            next_line_l = lines.index(line) + 1
+            if next_line_l < len(lines):
+                next_line = lines[next_line_l].strip()
+                if "$" in next_line:
+                    result["total"] = next_line
+            continue
+
+
+        if any(keyword in lower_line for keyword in skip_keywords): # skip irrelevant details on receipt. 
+            continue
+        
+
+        price_match = re.match(r"^\$[\d\.]+[A-Z]?$", clean_line) # searches for a dollar sign, one or more digits/decimal points, and one potential uppercase letter (accounting for the 'R' shown at the end of the prices on the receipt)
+
+        if price_match and last_line and not re.match(r"^\$[\d\.]+[A-Z]?$", last_line): # checks that the current line and the last line aren't both prices, rather the current one is a price and the last line is a statement. 
+            result["items"].append({
+                "name": last_line,
+                "price": re.sub(r"[A-Z]$", "", clean_line) # get rid the of 'R' at the end of the prices. Initially kept so that program does not skip over that line. 
+            })
+
+
+        last_line = clean_line # store the last line in the clean line for when it loops to the next iteration. Allows for comparison of next line with the current iteration.
+
     return result # return the final dictionary. 
 
 # Print JSON output
 parsed = parse_receipt(document) # feed the full text into our 'parse_receipt' function.
 print("\n--- Parsed JSON ---\n") 
 print(json.dumps(parsed, indent=2)) # converts python dictionary into json object. 
+
+with open("receipt_filtered.json", "w") as fileout: # save as JSON file. 
+    json.dump(parsed, fileout, indent = 2)
